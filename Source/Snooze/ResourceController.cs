3361806ba0298ad9f110473cab62731df5478069
Ryan Roberts <ryansroberts@gmail.com>
2010-07-12 10:42:25 +0100

    Resourceresult headers now implemented using object/tostring to allow use of urls when under test. Extend resourceresult to expose header enumeration for testing

--- Source/Snooze/ResourceController.cs
+++ Source/Snooze/ResourceController.cs
@@ -9,83 +9,100 @@ namespace Snooze
             ActionInvoker = new ResourceActionInvoker();
         }
 
+
         public virtual ResourceResult OK(object resource)
         {
             return new ResourceResult(200, resource);
         }
+
 
         public virtual ResourceResult Created(Url url, object informationResource)
         {
             return new ResourceResult(201, informationResource).WithHeader("Location", url.ToString());
         }
 
+
         public virtual ResourceResult NoContent()
         {
             return new ResourceResult(204, null);
         }
+
 
         public virtual ResourceResult ResetContent()
         {
             return new ResourceResult(205, null);
         }
 
+
         public virtual ResourceResult SeeOther(Url url)
         {
-            return new ResourceResult(303, null).WithHeader("Location", url.ToString());
+            return new ResourceResult(303, null).WithHeader("Location", url);
         }
+
 
         public virtual ResourceResult SeeOther(string url)
         {
             return new ResourceResult(303, null).WithHeader("Location", url);
         }
 
+
         public virtual ResourceResult NotModified()
         {
             return new ResourceResult(304, null);
         }
+
 
         public virtual ActionResult Redirect(Url url)
         {
             return Redirect(url.ToString());
         }
 
+
         public virtual ActionResult TemporaryRedirect(Url url)
         {
             return new ResourceResult(307, url.ToString()).WithHeader("Location", url.ToString());
         }
+
 
         public virtual ActionResult TemporaryRedirect(string url)
         {
             return new ResourceResult(307, url).WithHeader("Location", url);
         }
 
+
         public virtual ResourceResult BadRequest()
         {
             return new ResourceResult(400, null);
         }
+
 
         public virtual ResourceResult BadRequest(object errorResource)
         {
             return new ResourceResult(400, errorResource);
         }
 
+
         public virtual ResourceResult NotFound()
         {
             return new ResourceResult(404, null);
         }
+
 
         public virtual ResourceResult NotFound(object errorResource)
         {
             return new ResourceResult(404, null);
         }
 
+
         protected override void OnResultExecuting(ResultExecutingContext filterContext)
         {
             var r = filterContext.Result as ResourceResult;
+
             if (r != null)
             {
                 filterContext.Controller.ViewData.Model = r.Resource;
             }
+
 
             base.OnResultExecuting(filterContext);
         }
--- Source/Snooze/ResourceResult.cs
+++ Source/Snooze/ResourceResult.cs
@@ -8,133 +8,192 @@ namespace Snooze
 {
     public class ResourceResult : ActionResult
     {
+        readonly List<Action<HttpCachePolicyBase>> _cacheActions = new List<Action<HttpCachePolicyBase>>();
+        readonly List<HttpCookie> _cookies = new List<HttpCookie>();
+        readonly List<KeyValuePair<string, object>> _headers = new List<KeyValuePair<string, object>>();
+
+        public ILookup<string, object> Headers
+        {
+            get
+            {
+                return _headers.ToLookup(k => k.Key,v => v.Value);
+            }
+        }
+
         public ResourceResult(int statusCode, object resource)
         {
             StatusCode = statusCode;
+
             Resource = resource;
         }
 
+
         public int StatusCode { get; set; }
+
         public object Resource { get; set; }
+
         public string ContentType { get; set; }
 
-        List<KeyValuePair<string, string>> _headers = new List<KeyValuePair<string, string>>();
-        List<HttpCookie> _cookies = new List<HttpCookie>();
-        List<Action<HttpCachePolicyBase>> _cacheActions = new List<Action<HttpCachePolicyBase>>();
+
+        public ResourceResult WithHeader(string name, Url value)
+        {
+            _headers.Add(new KeyValuePair<string, object>(name, value));
+
+            return this;
+        }
+
 
         public ResourceResult WithHeader(string name, string value)
         {
-            _headers.Add(new KeyValuePair<string, string>(name, value));
+            _headers.Add(new KeyValuePair<string, object>(name, value));
+
             return this;
         }
+
 
         public ResourceResult WithCookie(HttpCookie cookie)
         {
             _cookies.Add(cookie);
+
             return this;
         }
+
 
         public ResourceResult WithCache(Action<HttpCachePolicyBase> action)
         {
             _cacheActions.Add(action);
+
             return this;
         }
+
 
         public ResourceResult AsJson()
         {
             ContentType = "application/json";
+
             return this;
         }
+
 
         public ResourceResult AsXml()
         {
             ContentType = "text/xml";
+
             return this;
         }
+
 
         public ResourceResult AsText()
         {
             ContentType = "text/plain";
+
             return this;
         }
+
 
         public ResourceResult As(string type)
         {
             ContentType = type;
+
             return this;
         }
+
 
         public ResourceResult AsFile(string type)
         {
             ContentType = type;
+
             return this;
         }
 
-        public ResourceResult AsFile(string type, string defaultFilename )
+
+        public ResourceResult AsFile(string type, string defaultFilename)
         {
             ContentType = type;
-            return this.WithHeader("Content-Disposition", "attachment; filename=" + defaultFilename);
+
+            return WithHeader("Content-Disposition", "attachment; filename=" + defaultFilename);
         }
+
 
         public override void ExecuteResult(ControllerContext context)
         {
             context.HttpContext.Response.StatusCode = StatusCode;
+
             AppendHeaders(context);
+
             AppendCookies(context);
+
             ApplyCacheActions(context);
+
 
             if (Resource == null) return;
 
+
             // Delegate to ActionResult, if one was given.
-            ActionResult innerResult = Resource as ActionResult;
+
+            var innerResult = Resource as ActionResult;
+
             if (innerResult != null)
             {
                 innerResult.ExecuteResult(context);
+
                 return;
             }
 
-            var acceptTypes = ParseAcceptTypes(context.HttpContext.Request.AcceptTypes);
-            var formatter = FindFormatter(context, acceptTypes);
+
+            IEnumerable<string> acceptTypes = ParseAcceptTypes(context.HttpContext.Request.AcceptTypes);
+
+            IResourceFormatter formatter = FindFormatter(context, acceptTypes);
+
             if (formatter == null)
             {
                 if (Resource is string)
                 {
                     context.HttpContext.Response.Output.Write(Resource);
                 }
+
                 else
                 {
                     context.HttpContext.Response.StatusCode = 406; // not acceptable
                 }
+
                 return;
             }
+
             formatter.Output(context, Resource, ContentType);
         }
 
+
         IEnumerable<string> ParseAcceptTypes(IEnumerable<string> types)
         {
-            // TODO process "q" and "level" options and sort accordingly
+            // TODO process "q" and "level" options and sort accordingly by stealing code from openrasta
+
             if (types == null) return Enumerable.Empty<string>();
+
             return from type in types
                    let pos = type.IndexOf(';')
                    let length = pos >= 0 ? pos : type.Length
                    select type.Substring(0, length);
         }
 
+
         void AppendCookies(ControllerContext context)
         {
-            foreach (var cookie in _cookies)
+            foreach (HttpCookie cookie in _cookies)
             {
                 context.HttpContext.Response.AppendCookie(cookie);
             }
         }
 
+
         void AppendHeaders(ControllerContext context)
         {
             foreach (var header in _headers)
             {
-                context.HttpContext.Response.AppendHeader(header.Key, header.Value);
+                context.HttpContext.Response.AppendHeader(header.Key, header.ToString());
             }
         }
+
 
         void ApplyCacheActions(ControllerContext context)
         {
@@ -144,21 +203,25 @@ namespace Snooze
             }
         }
 
+
         IResourceFormatter FindFormatter(ControllerContext context, IEnumerable<string> acceptTypes)
         {
             if (ContentType != null) // Controller action forced the content type.
             {
                 EnsureContentTypeIsMimeType();
+
                 return ResourceFormatters.Formatters.FirstOrDefault(f => f.CanFormat(context, Resource, ContentType));
             }
 
-            foreach (var formatter in ResourceFormatters.Formatters)
+
+            foreach (IResourceFormatter formatter in ResourceFormatters.Formatters)
             {
-                foreach (var acceptType in acceptTypes)
+                foreach (string acceptType in acceptTypes)
                 {
                     if (formatter.CanFormat(context, Resource, acceptType)) return formatter;
                 }
             }
+
             return null;
         }
 
