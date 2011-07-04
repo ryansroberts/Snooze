1513dcb1337b0e5193561e5a8a4ed2e7df52fa18
Brian Samson <bsamson@samtec.net>
2011-07-04 09:22:39 +0100

    moved the xml formatter above the bytearray formatter

--- src/Snooze/ResourceFormatters.cs
+++ src/Snooze/ResourceFormatters.cs
@@ -54,8 +54,8 @@ namespace Snooze
 
             defaultSerialisationFormatters.Add(new JsonFormatter());
             //adding the byte array formatter before the string formatter to support text plain files from byte array
-            defaultSerialisationFormatters.Add(new ByteArrayFormatter());
             defaultSerialisationFormatters.Add(new XmlFormatter());
+            defaultSerialisationFormatters.Add(new ByteArrayFormatter());
             defaultSerialisationFormatters.Add(new StringFormatter());
             
         }
