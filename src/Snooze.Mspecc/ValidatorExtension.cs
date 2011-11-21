e2a706ce1711953dc9b1287b31fe592ae7f43d1d
Ryan Roberts <ryansroberts@gmail.com>
2011-11-21 13:36:54 +0000

    Haack

--- src/Snooze.Mspecc/ValidatorExtension.cs
+++ src/Snooze.Mspecc/ValidatorExtension.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -19,6 +20,7 @@ namespace Snooze.MSpec
 			settings.ValidationEventHandler +=(s, e) => items.Add(e.Message);
 			settings.ValidationType = ValidationType.DTD;
 			settings.DtdProcessing = DtdProcessing.Parse;
+			settings.XmlResolver = new CachedXmlResolver();
 			
 			using (var reader = XmlReader.Create(new StringReader(doc.DocumentNode.OuterHtml),settings))
 			{
@@ -38,4 +40,52 @@ namespace Snooze.MSpec
 
 		}
 	}
+
+	public class CachedXmlResolver : XmlUrlResolver
+	{
+		public override Uri ResolveUri(Uri baseUri, string relativeUri)
+		{
+			if (relativeUri == "-//W3C//DTD XHTML 1.0 Strict//EN")
+				return new Uri("http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd");
+			else if (relativeUri == "-//W3C XHTML 1.0 Transitional//EN")
+				return new Uri("http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd");
+			else if (relativeUri == "-//W3C//DTD XHTML 1.0 Transitional//EN")
+				return new Uri("http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd");
+			else if (relativeUri == "-//W3C XHTML 1.0 Frameset//EN")
+				return new Uri("http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd");
+			else if (relativeUri == "-//W3C//DTD XHTML 1.1//EN")
+				return new Uri("http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd");
+			return base.ResolveUri(baseUri, relativeUri);
+		}
+
+		public override object GetEntity(Uri absoluteUri, string role, Type ofObjectToReturn)
+		{
+			if (!cache.ContainsKey(absoluteUri))
+				GetNewStream(absoluteUri, role, ofObjectToReturn);
+			return new FileStream(cache[absoluteUri], FileMode.Open, FileAccess.Read, FileShare.Read);
+		}
+
+		private void GetNewStream(Uri absoluteUri, string role, Type ofObjectToReturn)
+		{
+			using (Stream stream = (Stream)base.GetEntity(absoluteUri, role, ofObjectToReturn))
+			{
+				String filename = System.IO.Path.GetTempFileName();
+				using (FileStream ms = new FileStream(filename, FileMode.Create, FileAccess.Write))
+				{
+					Byte[] buffer = new byte[8192];
+					Int32 count = 0;
+					while ((count = stream.Read(buffer, 0, buffer.Length)) > 0)
+					{
+						ms.Write(buffer, 0, count);
+					}
+					ms.Flush();
+					cache.Add(absoluteUri, filename);
+				}
+			}
+		}
+
+		public static Dictionary<Uri, String> cache = new Dictionary<Uri, String>();
+
+	}
+
 }
--- src/Snooze/LeftMappingConfigurator.cs
+++ src/Snooze/LeftMappingConfigurator.cs
@@ -4,15 +4,15 @@ namespace Snooze
 {
 	public class LeftMappingConfigurator<TLeft>
 	{
-		TLeft item;
+		readonly TLeft item;
 
-		public LeftMappingConfigurator(TLeft item) 
-		{ 
-			this.item = item; 
+		public LeftMappingConfigurator(TLeft item)
+		{
+			this.item = item;
 		}
 
 		public RightConfigurator<TLeft, TRight> To<TRight>() where TRight : class {
-			return new RightConfigurator<TLeft, TRight>(item);	
+			return new RightConfigurator<TLeft, TRight>(item);
 		}
 
 		public RightConfigurator<TLeft, TRight> To<TRight>(Func<TLeft,TRight> projection) where TRight : class {
