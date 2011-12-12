c9248ee876c411880e5dd5e01c31819def90e053
Claire Hunter <claire.hunter@nice.org.uk>
2011-12-12 11:23:44 +0000

    first go at merging in to master

--- src/Snooze.Testing/Properties/AssemblyInfo.cs
+++ src/Snooze.Testing/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Snooze.Mspecc")]
+[assembly: AssemblyTitle("Snooze.Testing")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Snooze.Mspecc")]
+[assembly: AssemblyProduct("Snooze.Testing")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
