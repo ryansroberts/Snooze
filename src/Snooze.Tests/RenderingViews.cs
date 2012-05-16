b73b669e2b8779de17383b98313f5ffce87c4adb
vurt007 <vurt007@hotmail.com>
2012-05-16 16:00:15 +0100

    updated fizzler and htmlagilitypack

--- src/Snooze.Tests/RenderingViews.cs
+++ src/Snooze.Tests/RenderingViews.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web.Mvc;
 using Fizzler.Systems.HtmlAgilityPack;
 using Machine.Specifications;
 using SampleApplication.Controllers;
